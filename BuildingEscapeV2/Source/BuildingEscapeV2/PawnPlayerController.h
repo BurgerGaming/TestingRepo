3fdcccc5b720990dd9c68f264a18e30c7ce5e870
Daniel Vargas <daniel.vargas@LIVE.CA>
2018-12-01 09:53:22 -0500

    Important Stuff

---
+++ BuildingEscapeV2/Source/BuildingEscapeV2/PawnPlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PawnPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPEV2_API APawnPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
